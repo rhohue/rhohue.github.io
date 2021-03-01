a86a9438d020fced1c4e829ff96256439520b97b
*HUE* <68530454+rhohue@users.noreply.github.com>
2021-03-02 01:25:50 +0900

    Update banner.js

--- banner.js
+++ banner.js
@@ -5,7 +5,7 @@ const script = fs.readFileSync(filename);
 const padStart = str => ("0" + str).slice(-2);
 const dateObj = new Date();
 const date = `${dateObj.getFullYear()}-${padStart(
-  dateObj.getMonth() + 1
+  dateObj.getMonth()
 )}-${padStart(dateObj.getDate())}`;
 const banner = `/*!
  * Minimal Mistakes Jekyll Theme ${pkg.version} by ${pkg.author}
